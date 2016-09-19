24dea68c73cd7e1d206ad73cb89ea9164a25cf0e
ganeshbabu <ganeshbabu.atech@gmail.com>
2016-09-19 02:14:57 -0500

    intial commit

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector : 'hello-angular',
+	template : '<h1>This is Hello World Angular2 Application !!</h1>'
+})
+export class AppComponent { }
